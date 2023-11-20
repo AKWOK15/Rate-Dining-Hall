a70bbff69dbceb11011d3e28050eeb844da3a0a6
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2023-11-19 20:39:42 -0500

    Added star

--- rate-dining-hall/src/form.tsx
+++ rate-dining-hall/src/form.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './form.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faStar);
+
+
 
 function form() {
   return (
     <div className="form">
       <header className="form-header">
+      <FontAwesomeIcon icon="star" />
+      <FontAwesomeIcon icon="star" />
+      <FontAwesomeIcon icon="star" />
+      <FontAwesomeIcon icon="star" />
+      <FontAwesomeIcon icon="star" />
         <label>
-          Write your review: 
+          Write  review: 
           <textarea id="comment" name = "comment" rows = {3} cols = {30}></textarea>
         </label>
+        <input type= "submit" value= "submit"/>
       </header>
     </div>
   );
--- rate-dining-hall/src/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import form from './form';
-
-test('renders learn react link', () => {
-  render(<form />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
